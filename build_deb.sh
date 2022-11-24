6c4ce306a0173632978755ebd873ad1ed6201f85
Vladislav Ananev <vananyev@hystax.com>
2022-11-25 01:36:23 +0400

    install script

---
+++ build_deb.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+
+version=0.0-2
+
+echo "building deb for s3duck $version"
+
+if ! type "dpkg-deb" > /dev/null; then
+  echo "please install reauired build tools first"
+fi
+
+project="s3duck_${version}_amd64"
+folder_name="build/$project"
+echo "crating $folder_name"
+mkdir -p $folder_name
+cp -r DEBIAN/ $folder_name
+bin_dir="$folder_name/usr/bin"
+lib_dir="$folder_name/usr/lib/s3duck"
+mkdir -p $bin_dir
+mkdir -p $lib_dir
+cp s3duck $bin_dir
+cp -r icons/ $lib_dir
+cp resources/ducky.png $lib_dir
+cp resources/s3duck.desktop $lib_dir
+cp LICENSE $lib_dir
+cp *.py $lib_dir
+
+cd build/ && dpkg-deb --build --root-owner-group $project
+
+
+
+# mkdir s3duck_0.0-2_amd64
+# cd s3duck_0.0-2_amd64
+
+# dpkg-deb --build --root-owner-group s3duck_0.0-2_amd64
